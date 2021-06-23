d107a086d2adbe181a8a08e3c65348a182842d63
samitkumarsinha <samitkumarsinha@gmail.com>
2021-06-23 08:09:03 +0530

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { MyformComponent } from './myform/myform.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'myform', component: MyformComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/myform/myform.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-myform',
+  templateUrl: './myform.component.html',
+  styleUrls: ['./myform.component.css'],
+})
+export class MyformComponent implements OnInit {
+  myform = this.fb.group({
+    username: ['', Validators.required],
+    lessons: this.fb.array([]),
+  });
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+  get lessons() {
+    return this.myform.controls['lessons'] as FormArray;
+  }
+  addLesson() {
+    const lessonForm = this.fb.group({
+      title: ['', Validators.required],
+      level: ['beginner', Validators.required],
+    });
+
+    this.lessons.push(lessonForm);
+  }
+  deleteLesson(lessonIndex: number) {
+    this.lessons.removeAt(lessonIndex);
+  }
+}
